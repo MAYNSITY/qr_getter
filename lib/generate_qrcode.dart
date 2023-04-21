60faf669c14fa6bb01880040a8b6b3674b279e31
Mayank Garg <mayank@Maynsity.local>
2023-04-21 15:03:58 +0530

    api call added and visibility sorted

--- lib/generate_qrcode.dart
+++ lib/generate_qrcode.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+// Future<RawData> fetchRawData() async {
+//   final response = await http
+//       .get(Uri.parse('http://13.235.16.14/back/user-api/v0/test-raw-id/'));
+
+//   if (response.statusCode == 200) {
+//     // If the server did return a 200 OK response,
+//     // then parse the JSON.
+//     return RawData.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
+
+// class RawData {
+//   final String data;
+
+//   const RawData({
+//     required this.data,
+//   });
+
+//   factory RawData.fromJson(Map<String, dynamic> json) {
+//     return RawData(
+//       data: json['raw_id'],
+//     );
+//   }
+// }
 
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({super.key});
@@ -9,7 +41,18 @@ class GenerateQRCode extends StatefulWidget {
 }
 
 class GenerateQRCodeState extends State<GenerateQRCode> {
-  bool _Show = false;
+  String data = "";
+  bool display = false;
+  // List<dynamic> data = [];
+
+  // late Future<RawData> futureData;
+  // bool _Show = false;
+
+  // Future<http.Response> fetchRawdata() {
+  //   return http.get(
+  //     Uri.parse('http://13.235.16.14/back/user-api/v0/test-raw-id/'),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -27,36 +70,38 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
               height: 80,
             ),
             GestureDetector(
-              onTap: () {
-                setState(
-                  () {
-                    _Show = !_Show;
-                  },
-                );
-              },
+              onTap: getdata,
+              // onTap: () {
+              //   setState(
+              //     () {
+              //       _Show = !_Show;
+              //     },
+              //   );
+              // },
               child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.brown,
-                      borderRadius: BorderRadius.circular(12)),
-                  width: 280,
-                  height: 50,
-                  child: Center(
-                    child: Text(
-                      'Generate QR Code',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 25),
-                    ),
-                  )),
+                decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(12)),
+                width: 280,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Generate QR Code',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 25),
+                  ),
+                ),
+              ),
             ),
             SizedBox(
               height: 140,
             ),
             Visibility(
-              visible: _Show,
+              visible: display,
               child: QrImage(
-                data: 'http://13.235.16.14/back/user-api/v0/test-raw-id/',
+                data: data,
                 size: 300,
                 embeddedImageStyle: QrEmbeddedImageStyle(
                   size: const Size(
@@ -71,4 +116,39 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
       ),
     );
   }
+
+  void getdata() async {
+    print('1 call');
+    const url = 'http://13.235.16.14/back/user-api/v0/test-raw-id/';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      data = json['raw_id'];
+    });
+    display = true;
+    print(data);
+  }
+
+  // getData() {
+  //   futureData = fetchRawData();
+  //   print(futureData);
+  //   print("a");
+  // }
 }
+
+// List<dynamic> data = [];
+
+// void getdata() async {
+//   print('1 call');
+//   const url = 'http://13.235.16.14/back/user-api/v0/test-raw-id/';
+//   final uri = Uri.parse(url);
+//   final response = await http.get(uri);
+//   final body = response.body;
+//   final json = jsonDecode(body);
+//   setState(() {
+//     data = json['raw_data'];
+//   });
+//   print('2');
+// }
